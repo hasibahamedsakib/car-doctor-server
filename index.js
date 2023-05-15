f9c03dd112cc165d6c2691c99a8d7087366ff858
Hasib-Ahamed <hasibahamedsakib1@gamil.com>
2023-05-16 01:53:32 +0600

    added crud functionlity

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -7,7 +7,18 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 1010;
 
-const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@learntng.g96agpr.mongodb.net/?retryWrites=true&w=majority`;
+app.get("/", (req, res) => {
+  res.send("<h1>Server is running</h1>");
+});
+app.get("/ki", async (req, res) => {
+  // const result = await collection.find().toArray();
+  res.send("hello");
+});
+// const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@learntng.g96agpr.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb://car-doctor:IqBiDl1k3tH6CIC1@ac-jsn4phu-shard-00-00.g96agpr.mongodb.net:27017,ac-jsn4phu-shard-00-01.g96agpr.mongodb.net:27017,ac-jsn4phu-shard-00-02.g96agpr.mongodb.net:27017/?ssl=true&replicaSet=atlas-85j63k-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+// const uri =
+//   "mongodb+srv://car-doctor:IqBiDl1k3tH6CIC1@learntng.g96agpr.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -19,16 +30,63 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // await client.connect();
+    await client.connect();
+    const collection = client.db("CAR_DOCTOR").collection("services");
+    const bookings = client.db("CAR_DOCTOR").collection("bookings");
 
-    app.get("/", (req, res) => {
-      res.send("This is home page.");
+    app.get("/services", async (req, res) => {
+      const result = await collection.find().toArray();
+      res.send(result);
     });
 
-    app.post("/", (req, res) => {
-      res.send("This is home page with post request.");
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { title: 1, price: 1, description: 1, img: 1 },
+      };
+      const result = await collection.findOne(query, options);
+      res.send(result);
     });
 
+    app.get("/bookings", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookings.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+
+      const result = await bookings.insertOne(booking);
+      res.send(result);
+    });
+    // update booking
+    app.patch("/bookings/:id", async (req, res) => {
+      const updateBooking = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: updateBooking.status,
+        },
+      };
+      const result = await bookings.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+
+    // delete booking
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookings.deleteOne(query);
+      res.send(result);
+    });
+
+    await client.db("CAR_DOCTOR").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
